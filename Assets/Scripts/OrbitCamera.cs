fe71315aa9ea628885da890da0dba5cb97fff140
Dmitry Kaminsky <dimon_228_2@mail.ru>
2022-05-09 13:32:01 +0300

    Add move and jump(in test)

---
+++ Assets/Scripts/OrbitCamera.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    public float rotSpeed = 1.5f;
+    private float _rotY;
+    private Vector3 _offset; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rotY = transform.eulerAngles.y;
+        _offset = target.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        _rotY += Input.GetAxis("Mouse X") * rotSpeed * 3;
+        Quaternion rotaion = Quaternion.Euler(0, _rotY, 0);
+        transform.position = target.position - (rotaion * _offset);
+        transform.LookAt(target);
+    }
+}
