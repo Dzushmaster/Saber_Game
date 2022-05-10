929b0871b116635f5d0862556cfa3960eb81adc5
Dzushmaster <dimon_228_2@mail.ru>
2022-05-11 00:37:08 +0300

    Work double jump and dash

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,6 +8,8 @@ public class Movement : MonoBehaviour
     [SerializeField] float _jumpForce;
     [SerializeField] float _gravityScale;
     [SerializeField] float _countJumps;
+    [SerializeField] float _maxCountJumps;
+    [SerializeField] float _dashSpeed;
     Vector3 _moveDirection;
     private void Start()
     {
@@ -16,13 +18,25 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _moveDirection = new Vector3(_movementSpeed * Input.GetAxis("Horizontal"), _moveDirection.y, _movementSpeed * Input.GetAxis("Vertical"));
+        //_moveDirection = new Vector3(_movementSpeed * Input.GetAxis("Horizontal"), _moveDirection.y, _movementSpeed * Input.GetAxis("Vertical"));
+        float yStore = _moveDirection.y;
+        _moveDirection = -( transform.forward * Input.GetAxis("Vertical") * _movementSpeed+ transform.right * Input.GetAxis("Horizontal") * _movementSpeed);
+        //_moveDirection = _moveDirection.normalized * (-_movementSpeed);
+        _moveDirection.y = yStore;
+        if (Input.GetButtonDown("Jump") && _countJumps < _maxCountJumps)
+        {
+            _moveDirection.y = _jumpForce;
+            _countJumps++;
+        }
         if (_charController.isGrounded)
-            if (Input.GetButtonDown("Jump"))
-            {
-                _moveDirection.y = _jumpForce;
-                _countJumps++;
-            }
+            _countJumps = 0;
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _moveDirection = -(transform.forward * Input.GetAxis("Vertical") * _dashSpeed + transform.right * Input.GetAxis("Horizontal") * _dashSpeed);
+            //_moveDirection = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
+            //_moveDirection = _moveDirection.normalized * _dashSpeed;
+            _moveDirection.y = yStore;
+        }
         _moveDirection.y = _moveDirection.y + Physics.gravity.y * _gravityScale * Time.deltaTime;
         _charController.Move(_moveDirection * Time.deltaTime);
     }
--- Assets/Scripts/OrbitCamera.cs
+++ Assets/Scripts/OrbitCamera.cs
@@ -5,22 +5,37 @@ using UnityEngine;
 public class OrbitCamera : MonoBehaviour
 {
     [SerializeField] Transform target;
-    public float rotSpeed = 1.5f;
-    private float _rotY;
+    [SerializeField] Transform pivot;
+    float rotSpeed = 4.5f ;
     private Vector3 _offset; 
     // Start is called before the first frame update
     void Start()
     {
-        _rotY = transform.eulerAngles.y;
         _offset = target.position - transform.position;
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        _rotY += Input.GetAxis("Mouse X") * rotSpeed * 3;
-        Quaternion rotaion = Quaternion.Euler(0, _rotY, 0);
+        float _rotY = Input.GetAxis("Mouse X") * rotSpeed;
+        target.Rotate(0, _rotY, 0);
+
+        float _rotX = Input.GetAxis("Mouse Y")* rotSpeed;
+        pivot.Rotate(_rotX, 0, 0);
+
+        if (pivot.rotation.eulerAngles.x > 45f && pivot.rotation.eulerAngles.x < 180f)
+            pivot.rotation = Quaternion.Euler(45f, 0, 0);
+
+        float desiredYAngle = target.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+
+        Quaternion rotaion = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
         transform.position = target.position - (rotaion * _offset);
+        if (transform.position.y < target.position.y)
+            transform.position = new Vector3(transform.position.x, transform.position.y - .5f, transform.position.z);
         transform.LookAt(target);
     }
 }
