3947272659580a175a5dd0f328833565142cb258
Dzushmaster <dimon_228_2@mail.ru>
2022-05-16 08:13:16 +0300

    add script move and rotate platforms

---
+++ Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,27 @@
+using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    public MovePath _path;
+    [SerializeField] float speed;
+    [SerializeField] float _maxDistance = .1f;
+    private IEnumerator<Transform> _pointInPath;
+    // Update is called once per frame
+    private void Start()
+    {
+        _pointInPath = _path.GetNextPathPoint();
+        _pointInPath.MoveNext();
+        transform.position = _pointInPath.Current.position;
+    }
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _pointInPath.Current.position, Time.deltaTime * speed);
+        var distSquare = (transform.position - _pointInPath.Current.position).sqrMagnitude;
+        if (distSquare < _maxDistance * _maxDistance)
+            _pointInPath.MoveNext();
+    }
+
+}
