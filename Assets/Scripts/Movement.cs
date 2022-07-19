bff6951ea4f01e13fd62498761b82f94dab4a6b1
Dzushmaster <dimon_228_2@mail.ru>
2022-07-19 21:24:11 +0300

    add double dash ability

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,18 +2,16 @@ using System.Timers;
 using UnityEngine;
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
     [SerializeField] private float fallSpeed;
-    [SerializeField] private int maxCountJump;
-    [SerializeField] private float dashSpeed;
-    public Transform camera;
+    [SerializeField] Transform camera;
+    public float moveSpeed = 7;
     private float turnSmoothVelocity;
     private float turnSmoothTime = 0.1f;
-    private int countJump;
     private float playerSize = 2;
     private float groundDrag = 1;
-    private bool grounded;
+    public int countJump;
+    public bool grounded;
     private Vector3 moveDirection;
     private Rigidbody rb;
     private float horizontalInput;
@@ -53,13 +51,8 @@ public class Movement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
-        if (Input.GetKeyDown(KeyCode.Space) && maxCountJump > countJump + 1)
-        {
-            countJump++;
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
             Jump();
-        }
-        // if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
-        //     Dash();
     }
 
     private void MovePlayer(float angle)
@@ -69,15 +62,13 @@ public class Movement : MonoBehaviour
         {
             Vector3 moveDir = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
             transform.Translate(moveDir * moveSpeed * Time.fixedDeltaTime, Space.World);
-            //rb.velocity += moveDir.normalized * moveSpeed;
-            //rb.AddForce(moveDir.normalized * moveSpeed * 10f, ForceMode.Force);
         }
     }
-    private void Jump()
+    public void Jump()
     {
+        ++countJump;
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
         rb.velocity += Vector3.up * jumpForce;
-        //rb.AddForce(new Vector3(0, jumpHeight, 0), ForceMode.Impulse);
     }
 
     private void SpeedControl()
@@ -94,14 +85,4 @@ public class Movement : MonoBehaviour
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
         }
     }
-
-    // private void Dash()
-    // {
-    //     float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
-    //     float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-    //     transform.rotation = Quaternion.Euler(0f, angle, 0f);
-    //     Vector3 moveDir = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
-    //     //rb.velocity += moveDir.normalized * dashSpeed * 10f;
-    //     rb.AddForce(moveDir.normalized * dashSpeed * 10f, ForceMode.Impulse);
-    // }
 }
